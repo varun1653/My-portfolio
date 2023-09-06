9537ae916155e423b219015ecf5b4258e589bb24
Varun1653 <veeramallavarun3561@gmail.com>
2023-09-06 15:27:18 +0530

    initial commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
-window.onscroll = function() {
+window.onscroll = function () {
     const top = window.scrollY;
-    if(top >=100) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
     }
     else {
